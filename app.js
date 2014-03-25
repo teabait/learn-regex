23837304ceb2102ecce04ac7ae8ba1e854c8c984
Nathan Bashaw <nbashaw@gmail.com>
2014-03-25 10:19:55 -0400

    things

--- app.js
+++ app.js
@@ -1,51 +1,84 @@
-var lesson1Names = [
-  {
-    name: "Zorg",
-    enemy: true,
-    killed: false
-  },
-  {
-    name: "Blarog",
-    enemy: true,
-    killed: false
-  },
-  {
-    name: "Jeffrey",
-    enemy: false,
-    killed: false
-  }
-]
+$(function(){
 
-// The thing that checks User Submission and runs everything
-function checkRegex(userRegex, options, list) {
-  $('.flash').show().fadeOut();
-  var regex = new RegExp(userRegex, options);
-  list.forEach(function(creature){
-    var result = creature.name.match(regex);
-    if(result == creature.name){
-      creature.killed = true;
-      $('li[data-id=' + lesson1Names.indexOf(creature) + "]").addClass("killed");
+  var currentLesson = 0;
+  var transition = false;
+
+  // The thing that checks User Submission and runs everything
+  function checkRegex(userRegex, options, list) {
+    $('.flash').show().fadeOut();
+    var regex = new RegExp(userRegex, options);
+    list.forEach(function(person){
+      var result = person.name.match(regex);
+      if(result == person.name){
+        person.killed = true;
+        $('li[data-id=' + lessons[currentLesson].people.indexOf(person) + "]").addClass("killed");
+      }
+    });
+    $('.input').hide();
+    if(checkSuccess()) {
+      $('.pass').show();
+    } else {
+      $('.fail').show();
     }
-  })
-  checkSuccess();
-}
-
-// Returns true or false; determines whether user can go on to next lesson
-function checkSuccess () {
-  var enemiesAlive = _.where(lesson1Names, {enemy: true, killed: false});
-  var friendsKilled = _.where(lesson1Names, {enemy: false, killed: true});
-  if(enemiesAlive.length + friendsKilled.length === 0) {
-    return true;
-  } else {
-    return false;
+    setTimeout(function(){transition = true},100);
   }
-}
 
-$(document).ready(function(){
-  lesson1Names.forEach(function(creature){
-    $('.people').append("<li data-id='" + lesson1Names.indexOf(creature) + "' class='enemy-"+ creature.enemy + "'>" + creature.name + "</li>")
-  })
-  $("#submit").click(function(){
-    checkRegex($('#regex').val(), $('#options').val(), lesson1Names);
+  // Returns true or false; determines whether user can go on to next lesson
+  function checkSuccess() {
+    var enemiesAlive = _.where(lessons[currentLesson].people, {enemy: true, killed: false});
+    var friendsKilled = _.where(lessons[currentLesson].people, {enemy: false, killed: true});
+    if(enemiesAlive.length + friendsKilled.length === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  // Render a lesson stage
+  function renderLesson() {
+    var lesson = lessons[currentLesson];
+    $('.lesson-title').text(lesson.title);
+    $('.lesson-instructions').html(lesson.instructions);
+    $('.people').empty();
+    $('.input').show();
+    $('.after').hide();
+    $('input[type=text]').val('');
+    $('#regex').focus();
+    lesson.people.forEach(function(person){
+      $('.people').append("<li data-id='" + lesson.people.indexOf(person) + "' class='person enemy-"+ person.enemy + "'>" + person.name + "</li>");
+    });
+  }
+
+  function renderNextLesson() {
+    if (checkSuccess()) {
+      currentLesson += 1;
+    }
+    transition = false;
+    renderLesson();
+  }
+
+  // Proceed to next lesson
+  $('.next, .again').click(function(){
+    renderNextLesson();
   });
-})
+
+  $('body').on('keyup', function(e) {
+    if (transition && e.keyCode === 13) {
+      renderNextLesson();
+    }
+  });
+
+  // Check the code when someone clicks "submit"
+  $("#submit").click(function(){
+    checkRegex($('#regex').val(), $('#options').val(), lessons[currentLesson].people);
+  });
+  $('input[type=text]').on('keyup', function(e){
+    if (e.keyCode === 13) {
+      checkRegex($('#regex').val(), $('#options').val(), lessons[currentLesson].people);
+    }
+  });
+
+  // Kick off the app by rendering the first lesson
+  renderLesson();
+
+});
--- lessons.js
+++ lessons.js
@@ -1,6 +1,62 @@
 var lessons = [
-{
-  title: "Lesson 1"
-}
-]
+  {
+    title: "Leran Regular Expressions by Slaying Monsters!",
+    instructions: '<p>RegEx is a powerful language that lets you search for patterns in text. We\'ll use it to defeat the evil creatures that stand between us and Prince Cotter, who\'s trapped in the cave.</p><p>Defeat your first enemy by typing it\'s name, <code>Zord</code>, below.</p>',
+    people: [
+      {
+        name: 'Zord',
+        enemy: true,
+        killed: false
+      }
+    ]
+  },
+  {
+    title: 'Nice! You just wrote your first RegEx.',
+    instructions: 'But let\'s be honest, it was pretty weak. This time, kill both enemies at once by typing <code>.ord</code>',
+    people: [
+      {
+        name: 'Zord',
+        enemy: true,
+        killed: false
+      },
+      {
+        name: 'Bord',
+        enemy: true,
+        killed: false
+      }
+    ]
+  },
+  {
+    title: 'Whoa, what was that!?',
+    instructions: 'Regular Expressions have <i>metacharacters</i>, with special powers. You just used <code>.</code> which is like a wildcard, that matches any <i>single</i> character. Try it again! Write a RegEx that will match both <code>Zory</code> and <code>Zora</code>',
+    people: [
+      {
+        name: 'Zory',
+        enemy: true,
+        killed: false
+      },
+      {
+        name: 'Zora',
+        enemy: true,
+        killed: false
+      }
+    ]
+  },
+  {
+    title: 'You\'re getting the hang of it!',
+    instructions: 'Try and write a regex that matches both of these. Remember, <code>.</code> is a wildcard that will match any <i>single</i> character!',
+    people: [
+      {
+        name: 'Khan',
+        enemy: true,
+        killed: false
+      },
+      {
+        name: 'Sman',
+        enemy: true,
+        killed: false
+      }
+    ]
+  }
+];
 
