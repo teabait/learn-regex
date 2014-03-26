e3d2d4f9269f572aeb3c8dcdf2fa3e4477206eb0
teabait <hello.teaho@gmail.com>
2014-03-26 16:56:27 -0400

    changed lesson wording to be more explicit with instructions

--- lessons.js
+++ lessons.js
@@ -1,7 +1,7 @@
 var lessons = [
   {
     title: "Learn RegEx by Slaying Monsters!",
-    instructions: '<p>Regular Expressions (RegEx, for short) are powerful weapons that let you search for patterns in text. We\'ll use them in our quest to rescue Prince Cotter to match the name of the Monsters that are holding him captive!</p><p>Defeat your first enemy by typing its name, <code>Zord</code>, below.</p>',
+    instructions: '<p>Regular Expressions (RegEx, for short) are powerful weapons that let you search for patterns in text. We normally use them to validate email addresses, phone numbers, filter spreadsheets, and more!</p><p>But today we\'ll use them in our quest to rescue Prince Cotter to match the name of the monsters that are holding him captive!</p><p>To defeat your first enemy, Zord, you need to write a regex that matches his name. The simplest regex that matches every character in the name Zord is <code>Zord</code>.</p>',
     people: [
       {
         name: 'Zord',
@@ -14,7 +14,7 @@ var lessons = [
   },
   {
     title: 'Nice! You just wrote your first RegEx.',
-    instructions: 'But let\'s be honest, it was pretty weak. This time, kill both enemies at once by typing <code>.ord</code>',
+    instructions: 'But let\'s be honest, it was pretty weak. This time you need to defeat TWO monsters with ONE regex. You can use the wildcard character <code>.</code> to stand in for any single character. Kill both enemies at once by typing <code>.ord</code>',
     people: [
       {
         name: 'Ford',
@@ -32,8 +32,8 @@ var lessons = [
     hint: "Make sure to kill both monsters!"
   },
   {
-    title: 'Whoa, what was that!?',
-    instructions: 'Regular Expressions have <i>metacharacters</i>, with special powers. The one you just used (<code>.</code>) is a wildcard, that matches any <i>single</i> character. Try it again! Write a RegEx that will match both <code>Zory</code> and <code>Zora</code>',
+    title: 'You just used a metacharacter!',
+    instructions: 'The (<code>.</code>) you just used is a metacharacter &mdash; a symbol with special powers. Use it again! Write a RegEx that will match both <code>Zory</code> and <code>Zora</code>',
     people: [
       {
         name: 'Zory',
@@ -48,29 +48,10 @@ var lessons = [
         img: 'monsters/monster3b'
       }
     ],
-    hint: "Don't forget case sensitivity!"
+    hint: "Don't forget case sensitivity and be aware of the position of the wildcard."
   },
   {
-    title: 'You\'re getting the hang of it!',
-    instructions: 'Try and write a regex that matches both of these. Remember, <code>.</code> is a wildcard that will match any <i>single</i> character!',
-    people: [
-      {
-        name: 'Khan',
-        enemy: true,
-        killed: false,
-        img: 'monsters/monster4'
-      },
-      {
-        name: 'Sman',
-        enemy: true,
-        killed: false,
-        img: 'monsters/monster4b'
-      }
-    ],
-    hint: "The placement of the wildcard is a little different for this one! Try again!"
-  },
-  {
-    title: 'One more!',
+    title: 'More Monsters!',
     instructions: 'Try and write a regex that matches both of these. Remember, <code>.</code> is a wildcard that will match any <i>single</i> character!',
     people: [
       {
@@ -90,7 +71,7 @@ var lessons = [
   },
   {
     title: 'Great job! It\'s time to save Prince Cotter!',
-    instructions: 'Prince Cotter is being held captive by the monsters Bolter and Holker. To save Prince Cotter and win the game, defeat Bolter and Holker, but make sure not to hit the Prince!',
+    instructions: 'Prince Cotter is being held captive by the monsters Bolter and Holker. To save Prince Cotter and win the game, defeat Bolter and Holker, <strong>but make sure not to hit the Prince!</strong>',
     people: [
       {
         name: 'Bolter',
@@ -115,7 +96,7 @@ var lessons = [
   },
   {
     title: 'Oh no! Captured again!',
-    instructions: 'Prince Cotter has managed to get himself tangled up with a whole new breed of beastie! These new, tougher monsters have letters and numbers in their names. Defeat the monster ZL33t to free Cotter!',
+    instructions: 'Prince Cotter has managed to get himself tangled up with a whole new breed of beastie! These new, tougher monsters have letters and numbers in their names. Defeat the monster ZL33t to free Cotter! <strong>Don\'t hit the prince!</strong>',
     people: [
       {
         name: 'Cotter',
@@ -134,7 +115,7 @@ var lessons = [
   },
   {
     title: 'Save Blish!',
-    instructions: 'Blish is a friendly monster; he can help you find your way home if you save him from P4ish and 66ish. The metacharacter <code>.</code>\'s superpower is matching any character. But what if you only want to match numbers? Use <code>\\d</code> instead!',
+    instructions: 'Blish is a friendly monster; he can help you find your way home if you save him from P4ish and 66ish. The metacharacter <code>.</code>\'s superpower is matching any character. But what if you only want to match numbers? Use <code>\\d</code> instead! <strong>Remember: You don\'t want to hit the good guys!</strong>',
     people: [
       {
         name: 'Cotter',
